11291285458c99b552bb40b3e71a2733703166b9
mupartee <69388244+mupartee@users.noreply.github.com>
2020-08-28 12:46:01 +0300

    commit to master
    commit to Master

--- assets/testlogic.js
+++ assets/testlogic.js
@@ -37,41 +37,12 @@ $(document).ready(function() {
       .then( res => {
         res.json().then( data => {
           let coinPic = data[image].image;
-          $("<p/>").html(coinPic).appendTo(list).val();
+          $("<div></div>").appendTo(list).append(
+            `<img src=${coinPic}" width="25" height="25">`);
+          return coinPic;
         })
       })
   }
-
-/*
-  function createCoinTable(coinData) {
-    console.log('creating coin table');
-    // clear out any old elements
-    $('tr.content-row').remove();
-    const coinTable = $('#coinTable');
-
-    // create a table row for each coin in the list
-    for (let key in coinData) {
-        let coin = coinData[key];
-        // console.log(creating table row for: ${coin.name});
-        coinTable.append(
-            $('<tr class="content-row"></tr>').append(
-                $('<td class="text-left"></td>').text(coin.market_cap_rank),
-                $('<td class="text-left"></td>').append(
-                    $('<div></div>').append(
-                        <img src="${coin.image}" width="16" height="16"> ${coin.name}
-                    )),
-                $('<td ></td>').text(fmt.c0.format(coin.market_cap)),
-                $('<td></td>').text(fmt.c.format(coin.current_price)),
-                $('<td></td>').text(fmt.c0.format(coin.total_volume)),
-                $('<td></td>').text(fmt.n0.format(coin.circulating_supply)),
-                $(<td class="${coin.price_change_percentage_24h > 0 ? 'positive-change' : 'negative-change'}"></td>).text(fmt.p2.format(coin.price_change_percentage_24h / 100)),
-                $('<td></td>').text(''), // price graph?
-            )
-        );
-    }
-}
-*/
-
 
   for (var index = 0; index <= 9; index++) {
       getMarketCapName(index);
